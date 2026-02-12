1aecfb70bd6564a0dca5a235a4853109191d49e6
Emaniacinator <TeamVinton5@gmail.com>
2026-02-12 01:04:55 -0700

    Added footer to all pages

--- src/app.jsx
+++ src/app.jsx
@@ -4,5 +4,12 @@ import "tailwindcss/preflight";
 import "./app.css";
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="main-container">
+        <footer>
+            <p>Eric Vinton - CS 260</p>
+            <a href="https://github.com/Emaniacinator/startup">Click here for Github repo</a>
+        </footer>
+    </div>
+  );
 }
